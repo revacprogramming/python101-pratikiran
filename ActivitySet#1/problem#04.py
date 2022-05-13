40d8c4fa109f5133ebc2a86873d14c86d30081a4
Pratik K <undopatika@gmail.com>
2022-05-13 22:15:44 +0530

    p04 2 completed

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -16,3 +16,31 @@ if hrs > 40 :
     pay = pay + (hrs - 40) * (rate*0.5)
 
 print(pay)
+
+# Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. 
+# If the score is between 0.0 and 1.0, print a grade using the following table:
+# Score Grade
+# >= 0.9 A
+# >= 0.8 B
+# >= 0.7 C
+# >= 0.6 D
+# < 0.6 F
+# If the user enters a value out of range, print a suitable error message and exit. 
+# For the test, enter a score of 0.85.
+
+score = input("Enter Score : ")
+score = float(score)
+# print (score)
+
+if score > 1.00:
+    print("Value is of out of range. The value should be between (0, 1)")
+elif score >= 0.9:
+    print("A")
+elif score >= 0.80:
+    print("B")
+elif score >= 0.70:
+    print("C")
+elif score >= 0.60:
+    print("D")
+else:
+    print("F")
