334fb614b57ae1059d3f2b3951382192685a99d1
Pratik K <undopatika@gmail.com>
2022-05-13 20:59:09 +0530

    p03 completed

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,3 +1,13 @@
 # Variables, Expressions & Statements
 
-hrs = float(input("Enter hours? "))
+# Write a program to prompt the user for hours and rate per hour using input to compute gross pay.
+# Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25). 
+# You should use input to read a string and float() to convert the string to a number.
+# Do not worry about error checking or bad user data.
+
+hrs = input("Enter Hours : ")
+hrs = float(hrs)
+rate = input("Enter the rate per hour : ")
+rate = float(rate)
+pay = hrs * rate
+print("Pay:", pay)
