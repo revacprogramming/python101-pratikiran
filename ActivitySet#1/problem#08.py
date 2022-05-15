98aa5965d573c17eff2439987c4e7eb35b5849db
Pratik K <undopatika@gmail.com>
2022-05-15 21:52:15 +0530

    AC1 p08 completed

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,20 @@
 # Files
 
 filename = "dataset/mbox-short.txt"
+
+fh = open(filename)
+ans = 0
+count = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    pod = line
+    pod = pod.find(':')
+    pod = line[pod+1:]
+    pod = pod.strip()
+    pod = float(pod)
+    ans = ans + pod
+    count += 1
+average = ans/count
+print("Average spam confidence:", average)
+
