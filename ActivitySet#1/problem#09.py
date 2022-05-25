5d9c9e362f9fea00d95d870ebffb47f34bc73bb1
Pratik K <undopatika@gmail.com>
2022-05-25 15:50:32 +0530

    AC1 9,8,11

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,14 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    g=line.split()
+    for i in g:
+        if i not in lst:
+            lst.append(i)
+lst.sort()
+print(lst)
--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,26 @@
 # Tuples
 
 filename = "dataset/mbox-short.txt"
+
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+
+hrs = dict()
+
+for l in handle:
+    l = l.strip()
+    if 'From' in l:
+        if 'From:' not in l:
+            l = l.split()
+            for w in l:
+                if ':' in w:
+                    w = w[:2]
+                    if w in hrs:
+                        hrs[w] = hrs[w] + 1
+                    else :
+                        hrs[w] = 1
+                        
+for (k,v) in sorted(hrs.items()):
+    print(k,v)
