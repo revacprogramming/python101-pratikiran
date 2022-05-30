d25eb14e94f56bbaa3abe7823f3fc6f786c7695f
Pratik K <undopatika@gmail.com>
2022-05-31 00:01:10 +0530

    AC1 p12 completed

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -3,6 +3,16 @@
 
 import re
 
-f = open("dataset/romeo.txt", "r")
-x = re.search("^It.*sun$", f)
-print(f.read())
+x = "dataset/regex_sum_42.txt"
+z = "dataset/regex_sum_1548409.txt"
+
+sum = 0
+
+f = open(z)
+for line in f:
+    i = re.findall("[0-9]+", line)
+    if i != []:
+        for j in i:
+            sum += int(j)
+
+print(sum)
