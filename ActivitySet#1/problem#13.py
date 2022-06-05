5f68e0c3a66bd4385765fae407ef573be9d88511
Pratik K <undopatika@gmail.com>
2022-06-06 00:57:26 +0530

    AC1 not

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,4 +1,4 @@
 # Network Programming
 # https://www.py4e.com/lessons/network
 
-print("Sorry for not completing the task. Please")
+print("Sorry for not completing the task. Please can you comprehend the battle with free will and future permutations")
