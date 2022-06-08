ec0c55026548f430fcf77b5f6f1904e00f4e9b2c
Pratik K <undopatika@gmail.com>
2022-06-08 12:10:00 +0530

    AC1 p13 Completed

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,4 +1,17 @@
 # Network Programming
 # https://www.py4e.com/lessons/network
 
-print("Sorry for not completing the task. Please can you comprehend the battle with free will and future permutations")
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+mysock.close()
--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,7 +1,3 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
 
-i=[a for a in range(10)]
-c=i
-c.remove(3)
-print(c,i)
