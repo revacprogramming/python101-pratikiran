2bb1ddd0edda0c2a710f3bbffeacbe37923de093
Pratik K <undopatika@gmail.com>
2022-07-05 14:01:08 +0530

    AC2 p4 Completed

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -1,15 +1,28 @@
-
-
 def get_cs():
-    """get string input"""
+    a = input('Enter : ')
+    return a
 
 
 def cs_to_lot(cs):
-    """convert connected string to list of strings"""
+    data = list()
+    a = cs.split(';')
+    if '' in a:
+        a.remove('')
+    for ele in a:
+        t = ele.split('=')
+        t = tuple(t)
+        data.append(t)
+    return data
 
 
 def lot_to_cs(lot):
-    """convert list of strings to connected string"""
+    a = ''
+    for t in lot:
+        if t == lot[(len(lot)-1)]:
+            a = a+t[0]+'='+t[1]
+            continue
+        a = a+t[0]+'='+t[1]+';'
+    return a
 
 
 def main():
