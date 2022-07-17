9cec515f29007fc288fb73962252391aaff01162
Pratik K <undopatika@gmail.com>
2022-07-17 20:05:19 +0530

    Ac1 not completed

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,14 +1,31 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
 
-class roopa:
-    def __init__(self):
-        print('Goal of Science')
+class ceaser:
+    def __init__(self, shift, text):
+        self.shift = int(shift)
+        self.text = text
+    
+    def encrypt(self):
+        d = ''
+        for i in self.text:
+            if (i == ' '):
+                d += ' '
+            else:
+                d += (chr(ord(i) + self.shift))
+        return d
 
-    def gold(self):
-        return 1234
+    def decrypt(self):
+        d = ''
+        for i in self.text:
+            if (i == ' '):
+                d += ' '
+            else:
+                d += (chr(ord(i) - self.shift))
+        return d
 
-x = roopa()
-
-y = x.gold()
-print(y)
+text = 'What do you want to encrypt'
+a = ceaser(3, text)
+print(a.encrypt())
+a.text ='Zkdw gr |rx zdqw wr hqfu|sw'
+print(a.decrypt())
--- ActivitySet#1/problem#15.py
+++ ActivitySet#1/problem#15.py
@@ -1,6 +1,2 @@
 # Object Oriented Programming
 # https://www.py4e.com/lessons/Objects
-
-class pratik:
-    def __init__():
-        print('Hello')
