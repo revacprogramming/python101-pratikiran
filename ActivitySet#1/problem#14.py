ec8038d83fcd0f4650921560a840a7c6e94bb89a
Pratik K <undopatika@gmail.com>
2022-07-19 20:18:54 +0530

    Ac1 p4 completed

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -24,7 +24,31 @@ for comment in comments:
   count = int(comment.find('count').text)
   sum += count
 
-print(sum)
+print('XML =', sum)
 
 
-# JSON
+# JSON 
+
+import urllib.request, urllib.parse, urllib.error
+import json
+
+URL = 'http://py4e-data.dr-chuck.net/comments_1548414.json'
+
+fhand = urllib.request.urlopen(URL)
+
+json_data =''''''
+
+for line in fhand:
+  json_data += line.decode().strip() + '\n'
+
+json_dict = json.loads(json_data)
+
+comments = json_dict['comments']
+
+sum = 0
+
+for comment in comments:
+  count = int(comment['count'])
+  sum += count
+
+print('JSON =', sum)
